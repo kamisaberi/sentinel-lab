4fe410e640319af0e5708f7246afca8b580680e8
kamran saberifard <kamisaberi@gmail.com>
2026-09-13 00:53:16 +0330

    ebpf_filter.hpp  created

---
+++ include/sentinel_lab/sentinel_lab.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "sentinel_lab/event.hpp"
+#include "sentinel_lab/ring_buffer.hpp"
+#include "sentinel_lab/benchmarker.hpp"
+#include "sentinel_lab/engine.hpp"
+#include "sentinel_lab/ebpf_filter.hpp"
+#include "sentinel_lab/network_ingest.hpp"
